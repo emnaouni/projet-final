0548ef5866271c5599428d9a7a75cda2fce972a5
ladjimi1 <faroukladjimi@gmail.com>
2020-02-05 15:16:56 +0100

    docteur-reg

--- client/src/components/medecin/Register.js
+++ client/src/components/medecin/Register.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Register.css'
+import RegisterStepper from './Stepper';
 
- class Register extends Component {
+class Register extends Component {
     render() {
         return (
             <div className="regis">
                 <div className="compte">
-                    <h3>Crée mon compte</h3>
+                    <h3 className="text-comp">Crée mon compte</h3>
                     <div className="cree">
                         <p>Créer mon compte pour consulter un médecin en toute sécurité 24h/24 et 7j/7</p>
                         <div>
-                        <p>1- Je m'inscris</p>
-                        <p>2- Je renseigne mes informations</p>
-                        <p>3- Je consulte</p>
+                            <p>1- Je m'inscris</p>
+                            <p>2- Je renseigne mes informations</p>
+                            <p>3- Je consulte</p>
                         </div>
-                        
                     </div>
                     <p>J'ai déjà un compte</p>
                 </div>
                 <div className="key">
-                    <h2 className="tout">Tout d'abord, quelle est votre adresse e-mail ?</h2>
-                    <input className="inp" placeholder="EX:ex@gmail.com"/>
-
-                    <p className="accept">J'accepte les conditions générales d'utilisation</p>
-                    <p className="souhait">Je souhaite recevoir les conseils de santé par e-mail</p>
-                    <button className="confirm">Confirmer</button>
+                    <RegisterStepper />
                 </div>
-                
+
             </div>
         )
     }
